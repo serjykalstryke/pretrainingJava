28915b069354971c1252beb082fab8ce82f5ced8
David Stinnett <davidstinnett@icloud.com>
2023-05-12 21:10:23 -0400

    experimented with for loop

--- ForLoop.java
+++ ForLoop.java
@@ -10,6 +10,19 @@ public class ForLoop {
     for (int i = 0; i < arr.length; i++) {
       System.out.println(arr[i]);
     }
-  }
 
+    // Exercise 2
+    String[] cities = { "Atlanta", "Charlotte", "Dallas", "Los Angeles",
+        "New York", "Orlando", "Philadelphia", "Seattle" };
+    String[] reversedCities = new String[cities.length];
+
+    for (int i = 0; i < cities.length; i++) {
+      reversedCities[i] = cities[cities.length - 1 - i];
+
+      for (String city : reversedCities) {
+        System.out.println(city);
+      }
+    }
+
+  }
 }
