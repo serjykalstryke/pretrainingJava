d12917c6a85ea3c65b7cfcaac5628b9e89ee5edf
David Stinnett <davidstinnett@icloud.com>
2023-05-12 21:01:05 -0400

    experimented with for loop

---
+++ ForLoop.java
@@ -0,0 +1,15 @@
+public class ForLoop {
+  public static void main(String args[]) {
+    // Example for loop
+    for (int i = 0; i < 10; i++) {
+      System.out.println("i is currently: " + i);
+    }
+    // Exercise 1
+    byte[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+}
